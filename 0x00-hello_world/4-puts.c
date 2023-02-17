dba0eb2cbd458ada51f4d5aa9ecad61347daf653
TomUtopia <mnisi_tom@yahoo.com>
2023-02-17 08:57:35 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like buidling a multilingual puzzle");
+	return (0);
+}
+
+
+
+
+
+
+
+
+
