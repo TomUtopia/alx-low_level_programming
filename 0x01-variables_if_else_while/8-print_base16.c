aa17be0b71c48320290d5ace31049da4d30c46f0
TomUtopia <mnisi_tom@yahoo.com>
2023-02-20 20:39:57 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print the numbers in base 16
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	chat la;
+
+	for (num = 0; num < 10; numer++)
+	putchar((num % 10) + '0');
+
+	for (la = 'a'; la <= 'f'; la++)
+	putchar(la);
+
+	putchar('\n');
+
+	return (0);
+}
+
