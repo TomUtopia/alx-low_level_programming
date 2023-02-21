b137f1888f00e6747c12e9b0de3325bf4322d5b8
TomUtopia <mnisi_tom@yahoo.com>
2023-02-20 20:42:20 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,9 +7,9 @@
 int main(void)
 {
 	int num;
-	chat la;
+	char la;
 
-	for (num = 0; num < 10; numer++)
+	for (num = 0; num < 10; num++)
 	putchar((num % 10) + '0');
 
 	for (la = 'a'; la <= 'f'; la++)
