24b22e84d941190aa336a8f25d71007e7d6aab7a
TomUtopia <mnisi_tom@yahoo.com>
2023-02-28 08:56:35 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -14,7 +14,7 @@ int main(void)
     _putchar(' ');
     _putchar(r + '0');
     _putchar('\n');
-    r = print_sign(0);
+    r = print_sign(00);
     _putchar(',');
     _putchar(' ');
     _putchar(r + '0');
